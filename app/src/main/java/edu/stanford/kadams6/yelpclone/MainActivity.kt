21ebc2d76e469d6afc8c994bd5f4feab54ddc8e1
Kevin Adams <kevinadams@KevinBook-Pro.local>
2020-11-08 20:54:29 -0800

    Successfully issued GET request

--- app/src/main/java/edu/stanford/kadams6/yelpclone/MainActivity.kt
+++ app/src/main/java/edu/stanford/kadams6/yelpclone/MainActivity.kt
@@ -2,10 +2,38 @@ package edu.stanford.kadams6.yelpclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
+private const val TAG = "MainActivity"
+private const val BASE_URL = "https://api.yelp.com/v3/"
+private const val API_KEY = "2FQWE5RVIjQkDUVuQN6euPZsInVDoDXDdxr4atU-ZlVghAo1lxW_atMfZ0Or4tbMAk9cSG96wNZNYANrEjhXrVuTHMysI7DluRoGkjy9Cc4YeW4qTGD27Fqo_bmoX3Yx"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Create Retrofit instance
+        val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
+
+        // Define endpoint
+        val yelpService = retrofit.create(YelpService::class.java)
+
+        // Asynchronous network request
+        yelpService.searchRestaurants("Bearer $API_KEY", "Avocado Toast", "New York").enqueue(object: Callback<Any> {
+            override fun onResponse(call: Call<Any>, response: Response<Any>) {
+                Log.i(TAG, "onResponse $response")
+            }
+
+            override fun onFailure(call: Call<Any>, t: Throwable) {
+                Log.i(TAG, "onFailure $t")
+            }
+
+        })
+
     }
 }
---
+++ app/src/main/java/edu/stanford/kadams6/yelpclone/YelpService.kt
@@ -0,0 +1,15 @@
+package edu.stanford.kadams6.yelpclone
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface YelpService {
+
+    @GET("businesses/search")
+    fun searchRestaurants(
+        @Header("Authorization") authHeader: String,
+        @Query("term") term: String,
+        @Query("location") location: String) : Call<Any>
+}
